02f57b73c915bd2a00196a59d2351176e9dd07e6
Firebolt <24422634+Fireboltofdeath@users.noreply.github.com>
2021-01-07 23:47:25 -0500

    Fix findPrecedingIdentifier bug

--- src/util/functions/findPrecedingIdentifier.ts
+++ src/util/functions/findPrecedingIdentifier.ts
@@ -1,9 +1,16 @@
 import ts from "typescript";
 
-export function findPrecedingIdentifier(pos: number, sourceFile: ts.SourceFile): ts.Identifier | undefined {
+export function findPrecedingIdentifier(
+	pos: number,
+	sourceFile: ts.SourceFile,
+	isFirstIteration = true,
+): ts.Identifier | undefined {
 	const precedingToken = ts.findPrecedingToken(pos, sourceFile);
 	let precedingIdentifier: ts.Node | undefined;
 	if (precedingToken) {
+		if (ts.isIdentifier(precedingToken)) {
+			console.log("preceding token", precedingToken.text);
+		}
 		switch (precedingToken.kind) {
 			case ts.SyntaxKind.DotToken:
 			case ts.SyntaxKind.QuestionDotToken:
@@ -11,6 +18,9 @@ export function findPrecedingIdentifier(pos: number, sourceFile: ts.SourceFile):
 				precedingIdentifier = newPrecedingToken;
 				break;
 			case ts.SyntaxKind.Identifier:
+				if (isFirstIteration) {
+					return findPrecedingIdentifier(precedingToken.getFullStart() - 1, sourceFile, false);
+				}
 				precedingIdentifier = precedingToken;
 				break;
 		}
