dcf52161c391b5da15e97468ee89789caff74ac6
Firebolt <24422634+Fireboltofdeath@users.noreply.github.com>
2021-03-03 21:25:05 -0500

    Simplify getCompletionEntryDetails

--- src/languageService/getCompletionEntryDetails.ts
+++ src/languageService/getCompletionEntryDetails.ts
@@ -3,24 +3,27 @@ import { Provider } from "../util/provider";
 
 export function getCompletionEntryDetailsFactory(provider: Provider): ts.LanguageService["getCompletionEntryDetails"] {
 	const { service } = provider;
+
+	/**
+	 * Retrieve the text changes for a specific file given an array of code actions.
+	 * @param file The file to get changes for
+	 * @param codeActions The code actions
+	 */
+	function flattenChanges(file: string, codeActions: ts.CodeAction[]) {
+		return codeActions
+			.filter((x) => x.description.match(/^Import '.*' from module/))
+			.reduce((acc, val) => acc.concat(val.changes), new Array<ts.FileTextChanges>())
+			.filter((x) => x.fileName === file)
+			.reduce((acc, val) => acc.concat(val.textChanges), new Array<ts.TextChange>());
+	}
+
 	return (file, pos, entry, formatOptions, source, preferences) => {
 		const match = entry.match(/^(Server|Shared|Client): (\w+)$/);
 		if (match && match[2] && source) {
 			const result = service.getCompletionEntryDetails(file, pos, match[2], formatOptions, source, preferences);
 			if (result && result.codeActions && result.codeActions.length > 0) {
-				for (const x of result.codeActions) {
-					if (x.description.match(/^Import '.*' from module/)) {
-						for (const change of x.changes) {
-							if (change.fileName === file) {
-								for (const textChange of change.textChanges) {
-									textChange.newText = textChange.newText.replace(
-										/import {(.*)}/,
-										"import type {$1}",
-									);
-								}
-							}
-						}
-					}
+				for (const textChange of flattenChanges(file, result.codeActions)) {
+					textChange.newText = textChange.newText.replace(/import {(.*)}/, "import type {$1}");
 				}
 			}
 			return result;
