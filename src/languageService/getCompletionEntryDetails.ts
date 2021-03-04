f1d79f41c1e5f283100067040e196626ecca2650
Firebolt <24422634+Fireboltofdeath@users.noreply.github.com>
2021-03-03 21:06:09 -0500

    Remove unnecessary check

--- src/languageService/getCompletionEntryDetails.ts
+++ src/languageService/getCompletionEntryDetails.ts
@@ -1,5 +1,4 @@
 import ts from "typescript";
-import { getNetworkBoundary } from "../util/boundary";
 import { Provider } from "../util/provider";
 
 export function getCompletionEntryDetailsFactory(provider: Provider): ts.LanguageService["getCompletionEntryDetails"] {
@@ -9,19 +8,15 @@ export function getCompletionEntryDetailsFactory(provider: Provider): ts.Languag
 		if (match && match[2] && source) {
 			const result = service.getCompletionEntryDetails(file, pos, match[2], formatOptions, source, preferences);
 			if (result && result.codeActions && result.codeActions.length > 0) {
-				const boundary = getNetworkBoundary(provider, file);
-				const completionBoundary = getNetworkBoundary(provider, source);
-				if (boundary !== completionBoundary) {
-					for (const x of result.codeActions) {
-						if (x.description.match(/^Import '.*' from module/)) {
-							for (const change of x.changes) {
-								if (change.fileName === file) {
-									for (const textChange of change.textChanges) {
-										textChange.newText = textChange.newText.replace(
-											/import {(.*)}/,
-											"import type {$1}",
-										);
-									}
+				for (const x of result.codeActions) {
+					if (x.description.match(/^Import '.*' from module/)) {
+						for (const change of x.changes) {
+							if (change.fileName === file) {
+								for (const textChange of change.textChanges) {
+									textChange.newText = textChange.newText.replace(
+										/import {(.*)}/,
+										"import type {$1}",
+									);
 								}
 							}
 						}
